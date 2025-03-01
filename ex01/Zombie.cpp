0de59ac2c058eef988fffae6597959e0617d8f8a
Herinirina Livasoa Razafiandriatsima <hrazafia@c1r10s16.42antananarivo.mg>
2025-03-01 08:12:52 +0300

    update project

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrazafia <hrazafia@student.42antanana      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 07:53:26 by hrazafia          #+#    #+#             */
+/*   Updated: 2025/02/10 07:56:13 by hrazafia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name): _name(name)
+{
+
+}
+
+Zombie::~Zombie(void)
+{
+	std::cout << _name << std::endl;
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
