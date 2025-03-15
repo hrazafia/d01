82460e239ed879aae94b5bd114642dce33b69abc
Herinirina Livasoa Razafiandriatsima <hrazafia@c1r4s15.42antananarivo.mg>
2025-03-15 15:44:34 +0300

    update project

--- tests/test8/main.cpp
+++ tests/test8/main.cpp
@@ -6,13 +6,25 @@
 /*   By: hrazafia <hrazafia@student.42antanana      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 14:36:50 by hrazafia          #+#    #+#             */
-/*   Updated: 2025/03/15 15:08:01 by hrazafia         ###   ########.fr       */
+/*   Updated: 2025/03/15 15:43:03 by hrazafia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-void	debug(void)
+class	Harl
+{
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+
+	public:
+		void	complain(std::string level);
+};
+
+void	Harl::debug(void)
 {
 	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my ";
@@ -20,7 +32,7 @@ void	debug(void)
 	std::cout << "I really do!" << std::endl;
 }
 
-void	info(void)
+void	Harl::info(void)
 {
 	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. ";
@@ -28,7 +40,7 @@ void	info(void)
 	std::cout << "I wouldn’t be asking for more!" << std::endl;
 }
 
-void	warning(void)
+void	Harl::warning(void)
 {
 	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. ";
@@ -36,44 +48,41 @@ void	warning(void)
 	std::cout << "here since last month." << std::endl;
 }
 
-void	error(void)
+void	Harl::error(void)
 {
 	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now.";
 	std::cout << std::endl;
 }
 
-void	complain(std::string level)
+void	Harl::complain(std::string level)
 {
-	std::string	func[4] = {
-		"DEBUG",
-		"INFO",
-		"WARNING",
-		"ERROR"
-	};
+	Harl	harl;
 
-	void	(*complain[4])(void) = {
-		debug,
-		info,
-		warning,
-		error
+	std::string	func[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	void	(Harl::*complainPtr[4])(void) = {
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
 	};
 	int	i = 0;
 	while ((i < 4) && (level != func[i]))
 		i++;
-	if (i < 4)
-		complain[i]();
-	else
-		std::cout << "unknown level" << std::endl;
+	if ((i >= 0) && i < 4)
+		(harl.*complainPtr[i])();
 }
 
 int	main(int argc, char **argv)
 {
+	Harl	harl;
+
 	if (argc != 2)
 	{
-		std::cout << "one arguments are required" << std::endl;
+		std::cout << "only one argument is required" << std::endl;
 		return (1);
 	}
-	complain(argv[1]);
+	harl.complain(argv[1]);
 	return (0);
 }
